e892af013ae66b28b1ecb8ad174ec4e3e1a4eff1
faraday58 <jarodriguezvera@gmail.com>
2018-09-17 14:53:41 -0500

    Ejemplo con Musica

---
+++ ColeccionesG52019-I/Musica.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace ColeccionesG52019_I
+{
+    class Musica
+    {
+        private string autor;
+        private string album;
+        private string año;
+        private string titulo;
+
+        public Musica(string autor, string album, string año, string titulo)
+        {
+            this.autor = autor;
+            this.album = album;
+            this.año = año;
+            this.titulo = titulo;
+        }
+
+        public void Reproducir()
+        {
+            Console.WriteLine("Se está reproduciendo" + this.titulo + " - " + this.autor);
+
+        }
+
+        public void Pausar()
+        {
+            Console.WriteLine("Musica pausada");
+        }
+
+        public void Detener()
+        {
+            Console.WriteLine("Musica detenida");
+        }
+
+        public override string ToString()
+        {
+            string cancion;
+            cancion = titulo + " - " + autor;
+            return cancion;
+        }
+
+
+    }
+}
--- ColeccionesG52019-I/Program.cs
+++ ColeccionesG52019-I/Program.cs
@@ -1,15 +1,108 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
 namespace ColeccionesG52019_I
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            ArrayList colcadenas = new ArrayList();
+            colcadenas.Add("Uno");
+            colcadenas.Add("Dos");
+            colcadenas.Add("Tres");
+
+            foreach(string cadena in colcadenas)
+            {
+                Console.WriteLine(cadena);
+            }
+
+            ArrayList colnumeros = new ArrayList();
+            colnumeros.Add(1);
+            colnumeros.Add(2);
+            colnumeros.Add(3);
+
+            foreach( int numero in colnumeros   )
+            {
+                Console.WriteLine(numero);
+            }
+            
+            colnumeros.Add(3.1416f);
+            colnumeros.Add(123456789123456789);
+
+            for(int i=0; i < colnumeros.Count; i++ )
+            {
+                if( i < 3)
+                {
+                    int num = (int)colnumeros[i];
+                    Console.WriteLine(num);
+                }
+
+                if (i==3)
+                {
+
+                    float pi = (float)colnumeros[3];
+                    Console.WriteLine(pi);
+
+                }
+
+                if(i == 4)
+                {
+                    long num_1 = (long)colnumeros[4];
+                    Console.WriteLine(num_1.GetType());
+                }
+            }
+
+            colcadenas.Add("21");
+
+            int res = (int)(colnumeros[0]) + int.Parse(colcadenas[3].ToString());
+            Console.WriteLine("El resultado de {0} + {1} = {2}", colnumeros[0], colcadenas[3], res);
+
+            colcadenas.Remove("Dos");
+            Console.WriteLine(colcadenas[1]);
+
+            colcadenas.RemoveAt(2);
+            Console.WriteLine(colcadenas[1]);
+
+            colcadenas.Add("30");
+            colcadenas.Add("11");
+            colcadenas.Add("9");
+            colcadenas.Add(15);
+            colcadenas.Add("Treinta");
+
+
+
+            int pos = colcadenas.IndexOf("9");
+
+            int sum = int.Parse(colcadenas[pos].ToString()) + (int)colnumeros[2];
+            Console.WriteLine(sum);
+
+            Musica cancion1 = new Musica("The Beatles", "Abbey Road", "1969", "Abbey Road");
+            Musica cancion2 = new Musica("Avicii", "01", "2017", "Without You");
+            Musica cancion3 = new Musica("J Balvin", "No se", "2018", "Despacito");
+
+            ArrayList listacanciones = new ArrayList();
+
+            listacanciones.Add(cancion1);
+            listacanciones.Add(cancion2);
+            listacanciones.Add(cancion3);
+
+            Console.WriteLine("¿Qué canción quieres reproducir?");
+            int c = 0;
+            foreach(Musica can in listacanciones)
+            {
+                Console.WriteLine("[" + c + "]" + can.ToString());
+                c++;
+            }
+            c = int.Parse(Console.ReadLine());
+            Musica seleccionada = (Musica)listacanciones[c];
+            seleccionada.Reproducir();
+            seleccionada.Pausar();
+            seleccionada.Detener();
+            
+
+
+
         }
     }
 }
